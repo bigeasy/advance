142d797e2363129e75ee1238f3ebef2632e1fff8
Alan Gutierrez <alan@prettyrobots.com>
2013-12-26 20:36:45 -0800

    Optional fix up function.
    Allow an optional fix up function invoked as each element is visited.
    
    Closes #10.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-function Procede (array) {
+function echo (element) {
+    return element
+}
+
+function Procede (array, fixup) {
     this._array = array
+    this._fixup = fixup
     this._index = 0
 }
 
 Procede.prototype.next = function (callback) {
     if (this._index < this._array.length) {
-        callback(null, this._array[this._index++])
+        callback(null, this._fixup(this._array[this._index++]))
     } else {
         callback()
     }
@@ -14,6 +19,6 @@ Procede.prototype.next = function (callback) {
 Procede.prototype.unlock = function () {
 }
 
-exports.forward = function (array) {
-    return new Procede(array)
+exports.forward = function (array, fixup) {
+    return new Procede(array, fixup || echo)
 }
