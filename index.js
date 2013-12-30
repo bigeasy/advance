21056e7ef05c63203c186bca129e8fc8c3cd071f
Alan Gutierrez <alan@prettyrobots.com>
2013-12-30 00:47:23 -0800

    Rename project to Advance.
    Closes #13.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function Procede (array, fixup) {
+function Advance (array, fixup) {
     this._array = array
     this._fixup = fixup
     this._index = 0
 }
 
-Procede.prototype.next = function (callback) {
+Advance.prototype.next = function (callback) {
     if (this._index < this._array.length) {
         this._fixup(this._array[this._index++], callback)
     } else {
@@ -12,9 +12,9 @@ Procede.prototype.next = function (callback) {
     }
 }
 
-Procede.prototype.unlock = function () {
+Advance.prototype.unlock = function () {
 }
 
 exports.forward = function (array, fixup) {
-    return new Procede(array, fixup)
+    return new Advance(array, fixup)
 }
