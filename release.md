c1d95cd6195045cf49baa8b5841b742132e6c435
Alan Gutierrez <alan@prettyrobots.com>
2014-09-11 22:21:03 -0400

    Upgrade Proof to 0.0.47.
    Closes #35.

--- release.md
+++ release.md
@@ -1,5 +1,6 @@
 ### Issue by Issue
 
+ * Upgrade Proof to 0.0.47. #35.
  * Release version 0.0.6. #31.
  * Change "homepage" to GitHub Pages. #30.
  * Upgrade Proof to 0.0.44. #29.
--- t/advance/forward.t.js
+++ t/advance/forward.t.js
@@ -11,7 +11,7 @@ require('proof')(2, function (step, deepEqual) {
                 records.push(record)
                 keys.push(key)
             } else {
-                step(null)
+                return [ step ]
             }
         })()
     }, function () {
