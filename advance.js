7492cfaab66c770e7850f323ba8250d954fae1a5
Alan Gutierrez <alan@prettyrobots.com>
2015-02-22 07:18:06 -0600

    Implement new `get` interface.
    Closes #47.

--- advance.js
+++ advance.js
@@ -1,11 +1,18 @@
 function Advance (array) {
     this._array = array
+    this._index = 0
+}
+
+Advance.prototype.get = function () {
+    return this._array[this._index++]
 }
 
 Advance.prototype.next = function (callback) {
-    var array = this._array
-    this._array = null
-    callback(null, array)
+    if (this._index >= this._array.length) {
+        callback(null, false)
+    } else {
+        callback(null, true)
+    }
 }
 
 Advance.prototype.unlock = function (callback) {
--- t/advance/forward.t.js
+++ t/advance/forward.t.js
@@ -1,15 +1,20 @@
-require('proof')(2, require('cadence')(prove))
+require('proof')(3, require('cadence')(prove))
 
 function prove (async, assert) {
     var values = [ 'a', 'b', 'c' ]
     var iterator = require('../..')(values)
     async(function () {
         iterator.next(async())
-    }, function (items) {
+    }, function (more) {
+        assert(more, 'more')
+        var items = [], item
+        while (item = iterator.get()) {
+            items.push(item)
+        }
         assert(items, [ 'a', 'b', 'c' ], 'next')
         iterator.next(async())
-    }, function (items) {
-        assert(items, null, 'next null')
+    }, function (more) {
+        assert(!more, 'no more')
     }, function () {
         iterator.unlock(async())
     })
