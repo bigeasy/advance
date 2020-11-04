2c496a204c801969368dc7664d326e3367a74c6e
Alan Gutierrez <alan@prettyrobots.com>
2020-11-04 00:36:01 -0600

    Use MVCC constants.
    Use MVCC constants module for `FORWARD`, `REVERSE`, and `SET`.
    
    Closes #85.

--- advance.js
+++ advance.js
@@ -1,9 +1,11 @@
+const mvcc = require('mvcc')
+
 module.exports = function (arrays, { reverse = false, set = false } = {}) {
     if (reverse) {
         let index = arrays.length - 1
         const iterator = {
             done: false,
-            type: 1,
+            type: mvcc.REVERSE,
             next (promises, consume, terminator = iterator) {
                 if (index == -1) {
                     terminator.done = true
@@ -17,7 +19,7 @@ module.exports = function (arrays, { reverse = false, set = false } = {}) {
         let index = 0
         const iterator = {
             done: false,
-            type: set ? 2 : 0,
+            type: set ? mvcc.SET : mvcc.FORWARD,
             next (promises, consume, terminator = iterator) {
                 if (index == arrays.length) {
                     terminator.done = true
